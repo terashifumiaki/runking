fe0243b732783d3a3bcd3a5a7fca2c098ec5ed99
Terashi.Fumiaki <karakaraponpon@gmail.com>
2020-12-15 14:16:03 +0000

    add toppages navbar

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,6 @@
 class ToppagesController < ApplicationController
   def index
+    # 実は Controller のデフォルト機能として、アクションの最後に render :自身のアクション名 を呼び出しています。
+    # つまり今回は render :index であり、ToppagesController なので app/views/toppages/index.html.erb が呼び出される
   end
 end
