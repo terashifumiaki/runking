42d0bf505dc8dda8ba7af5c11425020f4584eb4b
Terashi.Fumiaki <karakaraponpon@gmail.com>
2020-12-16 14:29:29 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,29 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザ登録完了！'
+      # 成功でユーザーページへ
+      # redirect_to @user は、 処理を users#show のアクションへと強制的に移動させるもの
+      # create アクション実行後に更に show アクションが実行され、show.html.erb が呼ばれる。
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザ登録失敗！'
+      # 失敗でnew.html.erbへ（元の登録ページ）
+      render :new
+    end
+    # create に対応する View は用意する必要ない
   end
+
+
+  
+  private
+
+# Strong Paramter
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
