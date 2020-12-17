436cd1b0593eb0f22540b82ebf79ab9787313779
Terashi.Fumiaki <karakaraponpon@gmail.com>
2020-12-17 14:39:22 +0000

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 # root :to => 'コントローラ名#アクション名'
   root to: 'toppages#index'
   
+  # ログイン等の処理はセッション管理用のコントローラ sessions に記述
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # resources :users で生成されるURL である /users/new は少し格好悪く思えます。そんなときには get 'signup' のように個別に設定することで解決します
   get "signup", to: "users#new"
   resources :users, only:[:index, :show, :new, :create]
